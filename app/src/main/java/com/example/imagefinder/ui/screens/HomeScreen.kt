502029eeb30d673bd03b0036905446257895a387
aarav0012 <aaravjainjn@email.com>
2024-04-06 04:43:48 +0530

    add two screens

--- app/src/main/java/com/example/imagefinder/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/imagefinder/ui/screens/HomeScreen.kt
@@ -1,12 +1,31 @@
 package com.example.imagefinder.ui.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.imagefinder.ui.theme.ImageFinderTheme
 
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-
+    Column(modifier = modifier) {
+        Row {
+            Text(text = "Hey Hacker")
+        }
+    }
 }
 
 @Composable
-fun Pla
+fun PlagiarismChecker() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    ImageFinderTheme {
+        HomeScreen()
+    }
+}
--- app/src/main/java/com/example/imagefinder/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/imagefinder/ui/screens/LoginScreen.kt
@@ -1,26 +1,58 @@
 package com.example.imagefinder.ui.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.FlowRowScopeInstance.align
+
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.imagefinder.R
 import com.example.imagefinder.ui.theme.ImageFinderTheme
 
 @Composable
 fun LoginScreen(modifier: Modifier = Modifier) {
     Column(
-        modifier = modifier,
+        modifier = modifier
+            .background(color = colorResource(id = R.color.background_blue)),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     ) {
-        Text(
-            text = "Plagiarism Checker"
+
+        Image(
+            painter = painterResource(id = R.drawable.front_image),
+            contentDescription = "null",
+            modifier = Modifier.size(500.dp)
+                .padding(bottom = 16.dp)
         )
+
+
+        Text(
+            text = "Plagiarism Checker",
+            color = colorResource(id = R.color.text_blue),
+            fontSize = 30.sp,
+            modifier = Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(32.dp)
+        )
+
 
 //        Button(onClick = { /*TODO*/ }) {
 //            Text(
@@ -28,7 +60,11 @@ fun LoginScreen(modifier: Modifier = Modifier) {
 //            )
 //        }
 
-        Button(onClick = { /*TODO*/ }) {
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier.width(150.dp)
+                .height(50.dp)
+            ) {
             Text(text = "Sign In")
         }
 
