62b8f49a3c3046259a212ef66b85c6f519103068
Prit <pritbhanushali012@gmail.com>
2024-04-06 12:04:33 +0530

    created a card for data of api1

--- app/src/main/java/com/example/imagefinder/ui/screens/PlagiarismChecker.kt
+++ app/src/main/java/com/example/imagefinder/ui/screens/PlagiarismChecker.kt
@@ -1,16 +1,37 @@
 package com.example.imagefinder.ui.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.imagefinder.R
 import com.example.imagefinder.ui.theme.ImageFinderTheme
 
@@ -38,8 +59,76 @@ fun ImageFinderScreen(modifier: Modifier = Modifier) {
         LazyColumn {
 
         }
+    }
+}
 
+@Composable
+fun DetailCard(
+    modifier : Modifier = Modifier,
+) {
+    var expanded by remember { mutableStateOf(false) }
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Column(
+            modifier = modifier
+                .padding(8.dp),
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
+            Row(
+                modifier = modifier,
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "Domain icon",
+                    modifier = Modifier
+                        .size(50.dp)
+                )
 
+                Spacer(
+                    modifier = Modifier
+                    .width(20.dp)
+                )
+
+                Text(
+                    text = "Domain",
+                    modifier = modifier
+                )
+
+                Spacer(
+                    modifier = Modifier
+                        .weight(1f)
+                )
+
+                IconButton(
+                    onClick = { expanded = !expanded },
+                    modifier = modifier
+                ) {
+                    Icon(
+                        imageVector = if (expanded) Icons.Default.KeyboardArrowUp else Icons.Default.KeyboardArrowDown,
+                        contentDescription = "Expand"
+                    )
+                }
+            }
+
+            if(expanded) {
+                Text (
+                    text = "Title: " + "title",
+                )
+
+                Text (
+                    text = "Link: " + "URL",
+                )
+
+                Text (
+                    text = "Date: " + "date",
+                )
+            }
+        }
     }
 }
 
@@ -48,5 +137,13 @@ fun ImageFinderScreen(modifier: Modifier = Modifier) {
 fun ImageFinderScreenPreview() {
     ImageFinderTheme {
         ImageFinderScreen()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DetailCardPreview() {
+    ImageFinderTheme {
+        DetailCard()
     }
 }
