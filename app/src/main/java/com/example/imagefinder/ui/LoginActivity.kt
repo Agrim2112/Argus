d34f1ce6ee4ede50b71855b0419ed5abdc2c99fe
Agrim2112 <agrimgupta2112@gmail.com>
2024-04-07 03:24:36 +0530

    Added API

--- app/src/main/java/com/example/imagefinder/ui/LoginActivity.kt
+++ app/src/main/java/com/example/imagefinder/ui/LoginActivity.kt
@@ -1,12 +1,29 @@
 package com.example.imagefinder.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.imagefinder.R
+import com.example.imagefinder.databinding.ActivityLoginBinding
+import com.example.imagefinder.databinding.ActivitySignUpBinding
 
 class LoginActivity : AppCompatActivity() {
+    private var binding: ActivityLoginBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnLogin?.setOnClickListener()
+        {
+            val intent = Intent(this,CategorySelectionActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.tvSignUp?.setOnClickListener()
+        {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/example/imagefinder/ui/SignUpActivity.kt
+++ app/src/main/java/com/example/imagefinder/ui/SignUpActivity.kt
@@ -1,12 +1,31 @@
 package com.example.imagefinder.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.imagefinder.R
+import com.example.imagefinder.databinding.ActivityPlagiarismBinding
+import com.example.imagefinder.databinding.ActivitySignUpBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpActivity : AppCompatActivity() {
+    private var binding: ActivitySignUpBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnSignup?.setOnClickListener()
+        {
+            val intent = Intent(this,CategorySelectionActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding?.tvlogin?.setOnClickListener()
+        {
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
